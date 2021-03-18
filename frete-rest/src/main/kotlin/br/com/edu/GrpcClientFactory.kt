fd0991527efb3333566a7b38d4609798ce5f8cc2
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-03-18 17:06:47 -0300

    calculaFreteRest

--- frete-rest/src/main/kotlin/br/com/edu/GrpcClientFactory.kt
+++ frete-rest/src/main/kotlin/br/com/edu/GrpcClientFactory.kt
@@ -1,6 +1,23 @@
 package br.com.edu
 
+import br.com.zup.edu.FretesServiceGrpc
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+@Factory
 class GrpcClientFactory {
-
-
+    //@Bean // pra invocar o metodo
+    @Singleton
+    fun fretesClientStub(@GrpcChannel("fretes")
+                         channel : ManagedChannel): FretesServiceGrpc.FretesServiceBlockingStub? {
+//        val channel : ManagedChannel = ManagedChannelBuilder
+//            .forAddress("localhost", 50051)
+//            .usePlaintext()
+//            .maxRetryAttempts(10)
+//            .build()
+        return FretesServiceGrpc.newBlockingStub(channel)
+    }
 }
